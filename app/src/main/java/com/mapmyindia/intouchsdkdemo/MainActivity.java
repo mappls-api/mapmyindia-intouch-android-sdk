78359cd6769056cea6160264a432a47375c6f7b1
Deepak <deepak.kataria@mapmyindia.com>
2020-02-04 22:05:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/mapmyindia/intouchsdkdemo/MainActivity.java
@@ -0,0 +1,72 @@
+package com.mapmyindia.intouchsdkdemo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.mapmyindia.intouchsdkdemo.databinding.ActivityMainBinding;
+import com.mapmyindia.intouchsdkdemo.utils.PreferenceHelper;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            if (PreferenceHelper.getInstance().isInitialized(this)) {
+                replaceFragment(MainFragment.newInstance(), false);
+            } else {
+                replaceFragment(SetUpKeyFragment.newInstance(), false);
+            }
+        }, 3000);
+
+
+    }
+
+    public void replaceFragment(Fragment fragment, boolean addToBackStack) {
+        Fragment f = getSupportFragmentManager().findFragmentByTag(fragment.getClass().getName());
+        if (f == null) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(mBinding.mainContainer.getId(), fragment, fragment.getClass().getName());
+            if (addToBackStack) {
+                ft.addToBackStack(fragment.getClass().getName());
+            }
+            try {
+                ft.commit();
+            } catch (IllegalStateException e) {
+                ft.commitAllowingStateLoss();
+            }
+        } /*else {
+            // Log.d(TAG, "replaceFragment: Fragment " + fragment.getClass().getSimpleName() + " is Already Added. !!!");
+        }*/
+    }
+
+    public void addFragment(Fragment fragment, boolean addToBackStack) {
+        Fragment f = getSupportFragmentManager().findFragmentByTag(fragment.getClass().getName());
+        if (f == null) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(mBinding.mainContainer.getId(), fragment, fragment.getClass().getName());
+            if (addToBackStack) {
+                ft.addToBackStack(fragment.getClass().getName());
+            }
+            try {
+                ft.commit();
+            } catch (IllegalStateException e) {
+                ft.commitAllowingStateLoss();
+            }
+        } /*else {
+            // Log.d(TAG, "replaceFragment: Fragment " + fragment.getClass().getSimpleName() + " is Already Added. !!!");
+        }*/
+    }
+}
