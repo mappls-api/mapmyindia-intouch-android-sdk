ffb6ba99c0454b1d14126d1864ef08613c7c19e5
Deepak <deepak.kataria@mapmyindia.com>
2021-11-13 19:30:06 +0530

    - Updated Sdk dependency to 0.9.0.

--- app/src/main/java/com/mapmyindia/intouchsdkdemo/TrackingFragment.java
+++ app/src/main/java/com/mapmyindia/intouchsdkdemo/TrackingFragment.java
@@ -14,6 +14,7 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.mapmyindia.intouchsdkdemo.databinding.FragmentTrackingBinding;
+import com.mapmyindia.sdk.bleplugin.BluetoothLEScanHelper;
 import com.mapmyindia.sdk.intouch.InTouch;
 import com.mapmyindia.sdk.tracking.Config;
 import com.mapmyindia.sdk.tracking.TrackingStateObserver;
